3fdb8ed80830ccf82be44c14ae33bb3933853a02
Derry Ukere <ukderry@gmail.com>
2021-06-14 16:06:46 +0100

    completed navbar functionalities

--- src/components/atom/nav/MobileNav.tsx
+++ src/components/atom/nav/MobileNav.tsx
@@ -9,26 +9,24 @@ const MobileNav = (props :ToggleProps ) => {
   return (
     <nav>
       <ul className="navbar-nav" id="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link" href="#working-process">How it works</a>
+        <li className="nav-item" >
+          <a className="nav-link" href="#working-process" onClick = {props.clickHandlerTwo}>How  it  works</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#token-sale-details">Token details</a>
+          <a className="nav-link" href="#token-sale-details" onClick = {props.clickHandlerTwo} >Token details</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#roadmap">Roadmap</a>
+          <a className="nav-link" href="#roadmap" onClick = {props.clickHandlerTwo} >Roadmap</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#team">Team</a>
+          <a className="nav-link" href="#team"onClick = {props.clickHandlerTwo} >Team</a>
         </li>
+       
         <li className="nav-item">
-          <a className="nav-link" href="#documents">Documents</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#faq">FAQ</a>
-          <MemoizedBackdrop clickHandlerTwo = {props.clickHandlerTwo} state = {props.state}/>
+          <a className="nav-link" href="#faq" onClick = {props.clickHandlerTwo} >FAQ</a>
         </li>
       </ul>
+      <MemoizedBackdrop clickHandlerTwo = {props.clickHandlerTwo} state = {props.state}/>
     </nav>
     
   );
--- src/components/organism/Nav.tsx
+++ src/components/organism/Nav.tsx
@@ -1,4 +1,5 @@
-import React,{useState} from 'react';
+/* eslint-disable quotes */
+import React,{useState,useEffect} from 'react';
 import {MemoizedTogglerButtun} from '../atom/nav/TogglerButtun';
 import {MemoizedInternalMobileNav} from '../molecule/nav/InternalMobileNav';
 import {MemoizedBrand} from '../atom/nav/Brand';
@@ -6,7 +7,21 @@ import {MemoizedAuthen} from '../molecule/nav/Authen';
 
 const Nav = () => {
   const [sideDrawerOpen, setsideDrawerOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  let  close = "navbar navbar-sticky sticky-bg-color--dark navbar-expand-lg navbar-dark";
 
+
+  const handleScroll = () => {
+    const offset=window.scrollY;
+    offset > 200 &&  setScrolled (() => !scrolled);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll',handleScroll);
+    console.log('effect called');
+  },[]);
+
+  
   const openDrawer = ()=>{
     setsideDrawerOpen((prevState)=> !prevState);
   };
@@ -18,10 +33,14 @@ const Nav = () => {
   const backdroHandler = ()=> {
     setsideDrawerOpen((prevState)=> !prevState);
   };
+  
   // eslint-disable-next-line quotes
   const open = "navbar navbar-sticky sticky-bg-color--dark navbar-expand-lg navbar-dark active";
   // eslint-disable-next-line quotes
-  const close = "navbar navbar-sticky sticky-bg-color--dark navbar-expand-lg navbar-dark";
+  if(scrolled){
+    close = 'navbar navbar-sticky sticky-bg-color--dark navbar-expand-lg navbar-dark navbar-sticky--moved-up navbar-sticky--transitioned navbar-sticky--on';
+  } 
+
   return (
     <header className={sideDrawerOpen ? open : close}>
       <div className="container-fluid position-relative">
